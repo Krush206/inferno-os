1d6ef803bad09e09c1d85375dd4cce39168c758d
Matheus Garcia <37114863+Krush206@users.noreply.github.com>
2025-09-18 20:37:34 -0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,8 +1,16 @@
 load std
 
 echo 1 > /dev/jit
+echo ps2 > /dev/pointerctl
 9660srv /dev/sdD0/data /n/cd
 tarfs /n/cd/dis.tar /dis
 tarfs /n/cd/lib.tar /lib
 tarfs /n/cd/fonts.tar /fonts
 tarfs /n/cd/icons.tar /icons
+tarfs /n/cd/man.tar /man
+tarfs /n/cd/usr.tar /tmp
+memfs /chan
+memfs /usr
+cp -r /tmp/* /usr/
+unmount /tmp
+memfs
